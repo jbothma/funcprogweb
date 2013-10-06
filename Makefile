445ec883b19a995b6c69fd65c25ed9d38883c8e3
JD Bothma <jbothma@gmail.com>
2013-10-06 09:40:48 +0200

    Add dev-rel, add ws acceptor

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-all: rel
+all: compile
 
 get-deps:
 	./rebar get-deps
@@ -10,6 +10,9 @@ rel: compile
 	rm -rf rel/webdemo
 	./rebar generate
 
+dev-rel: rel
+	rm -rf rel/webdemo/lib/webdemo-1
+	ln -s `pwd`/apps/webdemo rel/webdemo/lib/webdemo-1
 clean:
 	./rebar clean
 	rm -rf rel/webdemo apps/*/ebin
--- apps/webdemo/src/webdemo_app.erl
+++ apps/webdemo/src/webdemo_app.erl
@@ -10,21 +10,34 @@
 %% ===================================================================
 
 start(_StartType, _StartArgs) ->
-    Routes = [{'_',
-               [{"/[...]", cowboy_static,
-                 [{directory, {priv_dir, webdemo, [<<"docroot">>]}},
-                  {mimetypes,
-                   [{<<".css">>, [<<"text/css">>]},
-                    {<<".js">>, [<<"application/javascript">>]},
-                    {<<".html">>, [<<"text/html">>]}
-                   ]}
-                 ]}
-               ]}],
-    Dispatch = cowboy_router:compile(Routes),
+    StaticRoutes =
+        [{'_',
+          [{"/[...]", cowboy_static,
+            [{directory, {priv_dir, webdemo, [<<"docroot">>]}},
+             {mimetypes,
+              [{<<".css">>, [<<"text/css">>]},
+               {<<".js">>, [<<"application/javascript">>]},
+               {<<".html">>, [<<"text/html">>]}
+              ]}
+            ]}
+          ]}],
+    StaticDispatch = cowboy_router:compile(StaticRoutes),
     %% Name, NbAcceptors, TransOpts, ProtoOpts
     cowboy:start_http(my_http_listener, 100,
                       [{port, 8002}],
-                      [{env, [{dispatch, Dispatch}]}]
+                      [{env, [{dispatch, StaticDispatch}]}]
+                     ),
+    WSRoutes =
+        [{'_',
+          [{"/[...]", ws_handler,
+            [
+            ]}
+          ]}],
+    WSDispatch = cowboy_router:compile(WSRoutes),
+    %% Name, NbAcceptors, TransOpts, ProtoOpts
+    cowboy:start_http(my_ws_listener, 100,
+                      [{port, 8001}],
+                      [{env, [{dispatch, WSDispatch}]}]
                      ),
     webdemo_sup:start_link().
 
