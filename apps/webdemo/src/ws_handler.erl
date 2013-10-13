691cb9c70616e48f101371712978c87b64ec97d9
JD Bothma <jbothma@gmail.com>
2013-10-13 21:32:23 +0200

    subscribe websocket procs to pusher simple pubsub server

--- apps/webdemo/src/ws_handler.erl
+++ apps/webdemo/src/ws_handler.erl
@@ -8,22 +8,19 @@
 -export([websocket_terminate/3]).
 
 init({tcp, http}, _Req, _Opts) ->
-	{upgrade, protocol, cowboy_websocket}.
+    {upgrade, protocol, cowboy_websocket}.
 
 websocket_init(_TransportName, Req, _Opts) ->
-	erlang:start_timer(1000, self(), <<"Hello!">>),
-	{ok, Req, undefined_state}.
+    ok = pusher:register(self()),
+    {ok, Req, undefined_state}.
 
-websocket_handle({text, Msg}, Req, State) ->
-	{reply, {text, << "That's what she said! ", Msg/binary >>}, Req, State};
 websocket_handle(_Data, Req, State) ->
-	{ok, Req, State}.
+    {ok, Req, State}.
 
-websocket_info({timeout, _Ref, Msg}, Req, State) ->
-	erlang:start_timer(1000, self(), <<"How' you doin'?">>),
-	{reply, {text, Msg}, Req, State};
+websocket_info({stats, Msg}, Req, State) ->
+    {reply, {text, Msg}, Req, State};
 websocket_info(_Info, Req, State) ->
-	{ok, Req, State}.
+    {ok, Req, State}.
 
 websocket_terminate(_Reason, _Req, _State) ->
-	ok.
+    ok.
