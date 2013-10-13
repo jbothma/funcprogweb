952395e970ef90f8106a52ab92b8006e488e0e36
JD Bothma <jbothma@gmail.com>
2013-10-13 21:31:53 +0200

    start pusher in supervisor

--- apps/webdemo/src/webdemo_sup.erl
+++ apps/webdemo/src/webdemo_sup.erl
@@ -23,5 +23,5 @@ start_link() ->
 %% ===================================================================
 
 init([]) ->
-    {ok, { {one_for_one, 5, 10}, []} }.
-
+    ChildSpecs = [?CHILD(pusher, worker)],
+    {ok, { {one_for_one, 5, 10}, ChildSpecs} }.
