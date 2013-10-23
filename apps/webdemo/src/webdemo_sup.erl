4577f71523d2bbfd512131ee526df2873ae535b8
JD Bothma <jbothma@gmail.com>
2013-10-23 23:02:25 +0200

    Add webmachine

--- apps/webdemo/src/webdemo_sup.erl
+++ apps/webdemo/src/webdemo_sup.erl
@@ -28,7 +28,15 @@ start_link() ->
 
 init([]) ->
     ChildSpecs = [?CHILD(pusher, worker),
-                  ?CHILD(worker_sup, supervisor)
+                  ?CHILD(worker_sup, supervisor),
+                  webmachine_instance_childspec()
                  ],
 
     {ok, { {one_for_one, 5, 10}, ChildSpecs} }.
+
+webmachine_instance_childspec() ->
+    Config = [{ip, "0.0.0.0"},
+              {port, 8003}
+              ],
+    {wm_instance, {webmachine_mochiweb, start, [Config]},
+     permanent, 5000, worker, dynamic}.
